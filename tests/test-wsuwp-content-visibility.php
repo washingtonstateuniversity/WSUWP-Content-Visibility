e045dad15a8dbac6fe586f38fd4c7893882593d0
Jeremy Felt <jeremy.felt@gmail.com>
2016-05-11 18:35:14 -0700

    Refactor tests to work with `user_can_read_post` method

--- tests/test-wsuwp-content-visibility.php
+++ tests/test-wsuwp-content-visibility.php
@@ -10,15 +10,11 @@ class Test_WSUWP_Content_Visibility extends WP_UnitTestCase {
 		$post_id = $this->factory->post->create( array( 'post_title' => 'Test Post', 'post_status' => 'private', 'post_author' => $user_id ) );
 
 		$post = get_post( $post_id );
-
-		$filtered_caps = $caps = array( 'will-not-change' );
-
-		$cap = 'read_post';
-		$args = array( $post->ID );
+		$user = get_user_by( 'id', $user_id );
 
 		$content_visibility = WSUWP_Content_Visibility();
 
-		$this->assertEquals( $filtered_caps, $content_visibility->allow_read_private_posts( $caps, $cap, $user_id, $args ) );
+		$this->assertTrue( $content_visibility->user_can_read_post(  $post, $user ) );
 	}
 
 	/**
@@ -31,46 +27,22 @@ class Test_WSUWP_Content_Visibility extends WP_UnitTestCase {
 		$user_id = $this->factory->user->create( array( 'user_login' => 'testeditor', 'role' => 'Editor' ) );
 
 		$post = get_post( $post_id );
+		$user = get_user_by( 'id', $user_id );
 
-		$caps = array( 'will-not-change' );
-		$cap = 'read_post';
-		$args = array( $post->ID );
-
-		update_post_meta( $post->ID, '_content_visibility_viewer_groups', array( 'group1' ) );
-
-		$content_visibility = WSUWP_Content_Visibility();
-
-		add_filter( 'user_in_content_visibility_groups', '__return_false' );
-		$actual_caps = $content_visibility->allow_read_private_posts( $caps, $cap, $user_id, $args );
-		remove_filter( 'user_in_content_visibility_groups', '__return_false' );
-
-		$this->assertEquals( $caps, $actual_caps );
-	}
-
-	/**
-	 * If a user is not a member of the content visibility groups for a post, and normally does not have access
-	 * to all private posts (Contributor), the list of caps passed to allow_read_private_posts will always be
-	 * returned untouched.
-	 */
-	public function test_contributor_user_in_content_visibility_groups_return_false() {
-		$post_id = $this->factory->post->create( array( 'post_title' => 'Test Post', 'post_status' => 'private' ) );
-		$user_id = $this->factory->user->create( array( 'user_login' => 'testeditor', 'role' => 'Contributor' ) );
-
-		$post = get_post( $post_id );
-
-		$caps = array( 'will-not-change' );
-		$cap = 'read_post';
-		$args = array( $post->ID );
+		$current_user_id = get_current_user_id();
+		wp_set_current_user( $user_id );
 
 		update_post_meta( $post->ID, '_content_visibility_viewer_groups', array( 'group1' ) );
 
 		$content_visibility = WSUWP_Content_Visibility();
 
 		add_filter( 'user_in_content_visibility_groups', '__return_false' );
-		$actual_caps = $content_visibility->allow_read_private_posts( $caps, $cap, $user_id, $args );
+		$can_read = $content_visibility->user_can_read_post( $post, $user );
 		remove_filter( 'user_in_content_visibility_groups', '__return_false' );
 
-		$this->assertEquals( $caps, $actual_caps );
+		wp_set_current_user( $current_user_id );
+
+		$this->assertFalse( $can_read );
 	}
 
 	/**
@@ -83,23 +55,22 @@ class Test_WSUWP_Content_Visibility extends WP_UnitTestCase {
 		$user_id = $this->factory->user->create( array( 'user_login' => 'testuser', 'role' => 'Contributor' ) );
 
 		$post = get_post( $post_id );
-		$post_type = get_post_type_object( $post->post_type );
+		$user = get_user_by( 'id', $user_id );
 
-		$caps = array( $post_type->cap->read_private_posts );
-		$filtered_caps = array( $post_type->cap->read );
-
-		$cap = 'read_post';
-		$args = array( $post->ID );
+		$current_user_id = get_current_user_id();
+		wp_set_current_user(  $user_id );
 
 		update_post_meta( $post->ID, '_content_visibility_viewer_groups', array( 'group1' ) );
 
 		$content_visibility = WSUWP_Content_Visibility();
 
 		add_filter( 'user_in_content_visibility_groups', '__return_true' );
-		$actual_caps = $content_visibility->allow_read_private_posts( $caps, $cap, $user_id, $args );
+		$can_read = $content_visibility->user_can_read_post( $post, $user );
 		remove_filter( 'user_in_content_visibility_groups', '__return_true' );
 
-		$this->assertEquals( $filtered_caps, $actual_caps );
+		wp_set_current_user(  $current_user_id );
+
+		$this->assertTrue( $can_read );
 	}
 
 	/**
@@ -111,20 +82,19 @@ class Test_WSUWP_Content_Visibility extends WP_UnitTestCase {
 		$user_id = $this->factory->user->create( array( 'user_login' => 'testsubscriber', 'role' => 'Subscriber' ) );
 
 		$post = get_post( $post_id );
-		$post_type = get_post_type_object( $post->post_type );
+		$user = get_user_by( 'id', $user_id );
 
-		$caps = array( $post_type->cap->read_private_posts );
-		$filtered_caps = array( $post_type->cap->read );
-
-		$cap = 'read_post';
-		$args = array( $post->ID );
+		$current_user_id = get_current_user_id();
+		wp_set_current_user( $user_id );
 
 		update_post_meta( $post->ID, '_content_visibility_viewer_groups', array( 'site-member' ) );
 
 		$content_visibility = WSUWP_Content_Visibility();
 
-		$actual_caps = $content_visibility->allow_read_private_posts( $caps, $cap, $user_id, $args );
+		$can_read = $content_visibility->user_can_read_post( $post, $user );
 
-		$this->assertEquals( $filtered_caps, $actual_caps );
+		wp_set_current_user( $current_user_id );
+
+		$this->assertTrue( $can_read );
 	}
 }
